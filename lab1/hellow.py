76158c87b5e60bc71e2ca8c2fc4a72ca221aecf2
Daneel Brychikov <BrychDaneel@mail.ru>
2017-02-25 00:21:54 +0300

    Первая лабораторная

---
+++ lab1/hellow.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+import sys
+
+def main():
+    print "Hello, World!"
+    print "This is {name}".format(name=sys.argv[0])
+
+if __name__ == "__main__":
+    main()
